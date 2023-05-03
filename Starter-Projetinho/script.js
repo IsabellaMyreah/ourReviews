d9154fd3531d7fe5278413a625bb9a6e63aaf349
Isabella Cavalcante <108815118+IsabellaMyreah@users.noreply.github.com>
2023-05-03 08:29:40 -0300

    Add files via upload

---
+++ Starter-Projetinho/script.js
@@ -0,0 +1,9 @@
+const reviews = [
+    {
+        id: 1,
+        name:"valquíria Soares",
+        job:"Web Developer",
+        image: "https://images.pexels.com/photos/1918246/pexels-photo-1918246.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        text: "I'm baby maggings twe health goth +1. Bicycle rights tumeric chartreuse before"
+    }
+]
